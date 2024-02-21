0f4caa37d04dc729d3d4f771a7cbf829357080a1
Palatip Jantawong <palatipjant@outlook.com>
2024-02-21 08:28:05 +0700

    Initial Commit

---
+++ Dek-D-Intern/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dek-D-Intern
+//
+//  Created by Palatip Jantawong on 21/2/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dek-D-Intern/Dek_D_InternApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dek_D_InternApp.swift
+//  Dek-D-Intern
+//
+//  Created by Palatip Jantawong on 21/2/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct Dek_D_InternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
